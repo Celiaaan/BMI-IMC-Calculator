7ea9fac694b63e973aaace2792b585c645a63f17
CCmel7 <57500079+CCmel7@users.noreply.github.com>
2021-05-23 19:34:27 +0200

    Update IMC.py

--- IMC.py
+++ IMC.py
@@ -15,7 +15,6 @@ from urllib import request
 from urllib.error import HTTPError
 
 
-
 def inputint(message):
     while True:
         try:
@@ -110,69 +109,99 @@ else:
 #######################################################################################################################
 #STRINGS FOR THE LOGS
 nowtxt = ("Date : " + str(now))
-IMCtxt = ("IMC : " + str(IMC))
 nametxt = ("Nom : " + str(name))
+agetxt = ("Age : " + str(age))
+IMCtxt = ("IMC : " + str(IMC))
 
 
 #CREATE THE LOGS FILES
 def yes_or_no(question):
     reply = str(input(question+' Oui ou non ? : ')).lower().strip()
-    if reply[0] in ('y','o'):
-        return True
-    if reply[0] in 'n':
-        return False
-    else:
+    try:
+        if reply[0] in ('y','o'):
+            return True
+        if reply[0] in 'n':
+            return False
+        else:
+            return yes_or_no("J'ai dit OUI ou NON ! : ")
+    except:
         return yes_or_no("J'ai dit OUI ou NON ! : ")
 
 log = yes_or_no(bcolors.ANY + bcolors.OKCYAN + "Voulez-vous envoyer le résultat dans logs.txt ? : ")
 if log == True:
     txt = open("logs.txt", "a+")
-    txt.writelines('\n'.join([separator, nametxt, nowtxt, IMCtxt, separator]) + '\n')
+    txt.writelines('\n'.join([separator, nowtxt, nametxt, agetxt, IMCtxt, separator]) + '\n')
 else: pass
+
+
+# La payload
+payload = {
+    'embeds': [
+        {
+            'title': 'Imc calc',  # Le titre de la carte
+            'description': ('\n'.join([nowtxt, nametxt, agetxt, IMCtxt]) + '\n'),  # Le corps de la carte
+            'image': {
+            'url': 'https://cdn.discordapp.com/attachments/835949614506573864/844244417867022366/calcul-IMC-indice-de-masse-corporelle-e1587390793294.png'
+            }
+        },
+
+    ]
+}
+
+# Les paramètres d'en-tête de la requête
+headers = {
+    'Content-Type': 'application/json',
+    'user-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'
+}
+
+
+def defreq():
+    try:
+        print(separator)
+        print('Un URL de webhook ressemble à ca : https://discord.com/api/webhooks/*****/*****')
+        req = request.Request(url=(input("Webhook URL : ")),
+                          data=json.dumps(payload).encode('utf-8'),
+                          headers=headers,
+                          method='POST')
+        return req
+    except:
+        return False
+
+
+# Puis on l'émet !
+def emeq(req):
+    if req == False:
+        print("Ce n'est pas un URL de webhook !")
+        return False
+    else:
+        try:
+            request.urlopen(req)
+            print('Webhook correct ! Envoi en cours...')
+            return True
+        except HTTPError as e:
+            print('ERREUR ! Webhook invalide')
+            print(e.reason)
+            return False
+        except:
+            print('ERREUR ! Webhook invalide')
+            return False
+
+
+
 
 web = yes_or_no(bcolors.OKCYAN + "Voulez-vous envoyer le résultat via discord ? ")
 
 if web == True:
-    WEBHOOK_URL = input("Webhook URL : ")
-    print(bcolors.ANY)
-    # La payload
-    payload = {
-        'embeds': [
-            {
-                'title': 'Imc calc',  # Le titre de la carte
-                'description': ('\n'.join([nametxt, nowtxt, IMCtxt]) + '\n'),  # Le corps de la carte
-                'image': {
-                    'url': 'https://cdn.discordapp.com/attachments/835949614506573864/844244417867022366/calcul-IMC-indice-de-masse-corporelle-e1587390793294.png'
-                    }
-            },
+    while emeq(defreq()) == False:
+            print("On recommence ! ")
 
-        ]
-    }
+    else:
+        pass
+else:
+    pass
 
-    # Les paramètres d'en-tête de la requête
-    headers = {
-        'Content-Type': 'application/json',
-        'user-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'
-    }
 
-    # Enfin on construit notre requête
-    req = request.Request(url=WEBHOOK_URL,
-                          data=json.dumps(payload).encode('utf-8'),
-                          headers=headers,
-                          method='POST')
 
-    # Puis on l'émet !
-    try:
-        response = request.urlopen(req)
-        print(response.status)
-        print(response.reason)
-        print(response.headers)
-    except HTTPError as e:
-        print('ERROR')
-        print(e.reason)
-        print(e.hdrs)
-        print(e.file.read())
-else: pass
 
 
 #######################################################################################################################
@@ -181,3 +210,4 @@ else: pass
 
 input(bcolors.OKCYAN + "Appuie  sur entrée pour terminer ")
 print('Au revoir !')
+
